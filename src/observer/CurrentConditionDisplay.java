34d98552b27089caf43a0c58722258a13d245f11
Sovat <ozzy_hl@mail.ru>
2019-06-15 16:45:50 +0500

    example of book-realization of observable pattern

---
+++ src/observer/CurrentConditionDisplay.java
@@ -0,0 +1,28 @@
+package observer;
+
+public class CurrentConditionDisplay implements Observer, DisplayElement {
+
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    private Subject weatherData;
+
+    public CurrentConditionDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity and " + pressure + "mm.");
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        this.temperature = temp;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        display();
+    }
+}
